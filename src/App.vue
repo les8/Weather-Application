9d5b2254034a3d29028a6192519abd23b6e91e88
leonid <leonid.skurihin@yandex.ru>
2022-03-13 13:32:37 +0300

    App.vue was changed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Weather application</h1>
+  <div class="wrapper">
+    <div class="header">
+      <Location />
+      <TemperatureScale />
+    </div>
+    <div class="main"></div>
+    <div class="footer"></div>
+  </div>
 </template>
 
 <script>
+import Location from "./components/header/location/Location.vue";
+import TemperatureScale from "./components/header/temperature-scale/Temperature-Scale.vue";
 export default {
   name: "App",
+  components: {
+    Location,
+    TemperatureScale,
+  },
 };
 </script>
