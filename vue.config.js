10b8dd41f1d1b1fe15e97719b2d30bc5afc5b14b
leonid <leonid.skurihin@yandex.ru>
2022-03-13 15:27:33 +0300

    Add svg plugins

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  chainWebpack: (config) => {
+    config.module.rule("svg-sprite").use("svgo-loader").loader("svgo-loader");
+  },
 });
