c7b9ef150b1e8b3122e3adfde5c5e55c8cf0e6d7
leonid <leonid.skurihin@yandex.ru>
2022-03-13 13:31:31 +0300

    Location.vue created

---
+++ src/components/header/location/Location.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="location">
+    <h3 class="location__name">Омск</h3>
+    <div class="location__managment">
+      <h6 class="location__change">Сменить город</h6>
+      <div class="location__mycoordinates">
+        <img src="" alt="Location icon" />
+        <p>Мое местоположение</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "location-block",
+};
+</script>
