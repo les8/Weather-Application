f607e0933bdb65846d511d303d155f5b22ba282a
leonid <leonid.skurihin@yandex.ru>
2022-03-16 17:54:22 +0300

    try to configurate svg-sprite #1

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,30 @@ module.exports = defineConfig({
   chainWebpack: (config) => {
     config.module.rule("svg-sprite").use("svgo-loader").loader("svgo-loader");
   },
+
+  pluginOptions: {
+    svgSprite: {
+      /*
+       * The directory containing your SVG files.
+       */
+      dir: "src/assets/svg",
+      /*
+       * The regex that will be used for the Webpack rule.
+       */
+      test: /\.(svg)(\?.*)?$/,
+      /*
+       * @see https://github.com/kisenka/svg-sprite-loader#configuration
+       */
+      loaderOptions: {
+        extract: true,
+        spriteFilename: "img/icons.[hash:8].svg", // or 'img/icons.svg' if filenameHashing == false это хз что за строка
+      },
+      /*
+       * @see https://github.com/kisenka/svg-sprite-loader#configuration
+       */
+      pluginOptions: {
+        plainSprite: true,
+      },
+    },
+  },
 });
