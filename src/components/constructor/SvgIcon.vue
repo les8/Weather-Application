c71caee97c04ce8f5486351025ff3d61eafb9372
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2022-03-27 20:50:28 +0300

    Change SvgIcon #1

--- src/components/constructor/SvgIcon.vue
+++ src/components/constructor/SvgIcon.vue
@@ -1,6 +1,5 @@
 <template>
   <svg :class="className" xmlns="http://www.w3.org/2000/svg">
-    <title v-if="title">{{ title }}</title>
     <use :xlink:href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink" />
   </svg>
 </template>
@@ -14,8 +13,7 @@ export default {
       type: String,
       required: true,
     },
-
-    title: {
+    className: {
       type: String,
       default: null,
     },
@@ -27,21 +25,8 @@ export default {
       if (Object.prototype.hasOwnProperty.call(icon, "default")) {
         icon = icon.default;
       }
-
       return icon.url;
-    },
-
-    className() {
-      return "svg-icon svg-icon--" + this.name;
     },
   },
 };
 </script>
-
-<style>
-.svg-icon {
-  fill: red;
-  height: 24px;
-  width: 24px;
-}
-</style>
