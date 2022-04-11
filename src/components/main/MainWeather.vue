45f61a120686b005b748e0e76b580a443fbac8aa
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2022-04-11 21:52:45 +0300

    Create MainWeather component #1

---
+++ src/components/main/MainWeather.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main__info">{{ this.currentTemperature }}</div>
+</template>
+
+<script>
+export default {
+  name: "main-weather",
+  data() {
+    return {
+      currentTemperature: "",
+      weatherInfo: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "/src/styles/variables.scss";
+</style>
