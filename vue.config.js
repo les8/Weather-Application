6788c51dd6c1e434e166ab68c4a9dcd1a91e9e8f
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2022-09-17 12:56:17 +0300

    delete cooments #19

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
@@ -8,24 +9,12 @@ module.exports = defineConfig({
 
   pluginOptions: {
     svgSprite: {
-      /*
-       * The directory containing your SVG files.
-       */
       dir: "src/assets/svg",
-      /*
-       * The regex that will be used for the Webpack rule.
-       */
       test: /\.(svg)(\?.*)?$/,
-      /*
-       * @see https://github.com/kisenka/svg-sprite-loader#configuration
-       */
       loaderOptions: {
         extract: true,
-        spriteFilename: "img/icons.[hash:8].svg", // or 'img/icons.svg' if filenameHashing == false это хз что за строка
+        spriteFilename: "img/icons.[hash:8].svg",
       },
-      /*
-       * @see https://github.com/kisenka/svg-sprite-loader#configuration
-       */
       pluginOptions: {
         plainSprite: true,
       },
